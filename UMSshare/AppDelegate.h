3b17c927f7926050d4d8eebd0364c358b463f3bc
xiaoyin <xiaoyin@macs-macbook-pro-2.local>
2016-05-27 11:39:41 +0800

    Initial Commit

---
+++ UMSshare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UMSshare
+//
+//  Created by xiaoyin on 16/5/27.
+//  Copyright © 2016年 xiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UMSshare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UMSshare
+//
+//  Created by xiaoyin on 16/5/27.
+//  Copyright © 2016年 xiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
